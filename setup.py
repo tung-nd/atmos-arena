cb40c3e48da8b79341f0897705cfdabc1147fd0d
Tung Nguyen <tungnd@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2024-05-24 18:07:36 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atmos-arena',
+    version='0.0.1',  # Your package version
+    description='atmos-arena',
+    author='Tung Nguyen',
+    author_email='ductungnguyen1997@gmail.com',
+    url='https://github.com/tung-nd/atmos-arena',  # Optional
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        # Choose your license as you wish
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
