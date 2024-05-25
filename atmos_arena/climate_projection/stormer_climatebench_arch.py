65bbe0070a26a9aac381e52a94099b083eab51e4
Tung Nguyen <tungnd@x3006c0s13b1n0.hsn.cm.polaris.alcf.anl.gov>
2024-05-25 05:16:12 +0000

    add climatebench task

---
+++ atmos_arena/climate_projection/stormer_climatebench_arch.py
@@ -0,0 +1,96 @@
+#Third Party
+import torch
+import torch.nn as nn
+import numpy as np
+from stormer_arch import Stormer
+from atmos_utils.pos_embed import get_1d_sincos_pos_embed_from_grid
+
+
+class StormerClimateBench(Stormer):
+    def __init__(self, 
+        in_img_size,
+        in_variables,
+        out_variables,
+        time_history=10,
+        patch_size=2,
+        embed_norm=True,
+        hidden_size=1024,
+        depth=24,
+        num_heads=16,
+        mlp_ratio=4.0,
+        freeze_encoder=True
+    ):
+        assert out_variables is not None
+        super().__init__(
+            in_img_size,
+            in_variables,
+            out_variables,
+            patch_size,
+            embed_norm,
+            hidden_size,
+            depth,
+            num_heads,
+            mlp_ratio,
+        )
+        
+        self.time_history = time_history
+        self.freeze_encoder = freeze_encoder
+        
+        # used to aggregate multiple timesteps in the input
+        self.time_pos_embed = nn.Parameter(torch.zeros(1, time_history, hidden_size), requires_grad=True)
+        self.time_agg = nn.MultiheadAttention(hidden_size, num_heads, batch_first=True)
+        self.time_query = nn.Parameter(torch.zeros(1, 1, hidden_size), requires_grad=True)
+        
+        # initialize time embedding
+        time_pos_embed = get_1d_sincos_pos_embed_from_grid(self.time_pos_embed.shape[-1], np.arange(self.time_history))
+        self.time_pos_embed.data.copy_(torch.from_numpy(time_pos_embed).float().unsqueeze(0))
+        
+        # # overwrite Stormer
+        # # use a linear prediction head for this task
+        # self.head = nn.Linear(hidden_size, in_img_size[0]*in_img_size[1])
+
+        if freeze_encoder:
+            for name, p in self.blocks.named_parameters():
+                name = name.lower()
+                # we do not freeze the norm layers, as suggested by https://arxiv.org/abs/2103.05247
+                if 'norm' in name:
+                    continue
+                else:
+                    p.requires_grad_(False)
+    
+    def aggregate_time(self, x: torch.Tensor):
+        """
+        x: B, T, L, D
+        """
+        b, _, l, _ = x.shape
+        x = torch.einsum("btld->bltd", x)
+        x = x.flatten(0, 1)  # BxL, T, D
+
+        time_query = self.time_query.repeat_interleave(x.shape[0], dim=0) # BxL, 1, D
+        x, _ = self.time_agg(time_query, x, x)  # BxL, 1, D
+        x = x.squeeze(1) # BxL, D
+
+        return x.unflatten(dim=0, sizes=(b, l))  # B, L, D
+
+    def forward(self, x, time_interval, variables):
+        # x: `[B, T, V, H, W]` shape.
+        b, t, _, _, _ = x.shape
+        x = x.flatten(0, 1)  # BxT, V, H, W
+        
+        x = self.embedding(x, variables) # BxT, L, D
+        x = self.embed_norm_layer(x) # BxT, L, D
+        
+        # add time embedding and aggregate over time dimension
+        # time emb: 1, T, D
+        x = x.unflatten(0, sizes=(b, t)) # B, T, L, D
+        x = x + self.time_pos_embed.unsqueeze(2)
+        x = self.aggregate_time(x) # B, L, D
+
+        time_interval_emb = self.t_embedder(time_interval) # time_interval_emb is an artifact from the pretrained model
+        for block in self.blocks:
+            x = block(x, time_interval_emb)
+        
+        x = self.head(x, time_interval_emb)
+        x = self.unpatchify(x)
+        
+        return x
