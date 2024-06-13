a3049b402484ee15ef2654ac444a291551e70e4a
Tung Nguyen <tungnd@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2024-06-13 07:26:16 +0000

    data processing for cams

---
+++ atmos_arena/data_processing/compute_cams_normalization.py
@@ -0,0 +1,177 @@
+import os
+import numpy as np
+import xarray as xr
+from glob import glob
+from tqdm import tqdm
+from atmos_utils.data_utils import (
+    SINGLE_LEVEL_VARS,
+    PRESSURE_LEVEL_VARS,
+    DEFAULT_PRESSURE_LEVELS,
+    NAME_TO_VAR,
+    CHEMISTRY_VARS
+)
+
+root_dir = '/eagle/MDClimSim/tungnd/data/cams_1.40625'
+save_dir = '/eagle/MDClimSim/tungnd/data/cams_1.40625_one_step_h5df'
+var_list = [
+    "2m_temperature",
+    "10m_u_component_of_wind",
+    "10m_v_component_of_wind",
+    "mean_sea_level_pressure",
+    "particulate_matter_10um",
+    "particulate_matter_1um",
+    "particulate_matter_2.5um",
+    "total_column_carbon_monoxide",
+    "total_column_nitrogen_dioxide",
+    "total_column_nitrogen_monoxide",
+    "total_column_ozone",
+    "total_column_sulphur_dioxide",
+    
+    # "carbon_monoxide",
+    # "geopotential",
+    # "nitrogen_dioxide",
+    # "nitrogen_monoxide",
+    "ozone",
+    "specific_humidity",
+    "sulphur_dioxide",
+    # "temperature",
+    "u_component_of_wind",
+    "v_component_of_wind",
+]
+years = range(2017, 2021)
+chunk_size = 100
+
+list_single_vars = [v for v in var_list if v in SINGLE_LEVEL_VARS]
+list_pressure_vars = [v for v in var_list if v in PRESSURE_LEVEL_VARS]
+
+if not os.path.exists(os.path.join(save_dir, f"normalize_mean.npz")):
+    normalize_mean = {}
+    normalize_std = {}
+    normalize_log_mean = {}
+    normalize_log_std = {}
+
+    for var in list_single_vars:
+        normalize_mean[var] = []
+        normalize_std[var] = []
+        if var in CHEMISTRY_VARS:
+            normalize_log_mean[var] = []
+            normalize_log_std[var] = []
+    for var in list_pressure_vars:
+        for level in DEFAULT_PRESSURE_LEVELS:
+            normalize_mean[f'{var}_{level}'] = []
+            normalize_std[f'{var}_{level}'] = []
+            if var in CHEMISTRY_VARS:
+                normalize_log_mean[f'{var}_{level}'] = []
+                normalize_log_std[f'{var}_{level}'] = []
+else:
+    normalize_mean = np.load(os.path.join(save_dir, f"normalize_mean.npz"))
+    normalize_std = np.load(os.path.join(save_dir, f"normalize_std.npz"))
+    normalize_log_mean = np.load(os.path.join(save_dir, f"normalize_log_mean.npz"))
+    normalize_log_std = np.load(os.path.join(save_dir, f"normalize_log_std.npz"))
+    normalize_mean = {k: list(v) for k, v in normalize_mean.items()}
+    normalize_std = {k: list(v) for k, v in normalize_std.items()}
+    normalize_log_mean = {k: list(v) for k, v in normalize_log_mean.items()}
+    normalize_log_std = {k: list(v) for k, v in normalize_log_std.items()}
+
+for var in tqdm(list_single_vars + list_pressure_vars, desc='variables', position=0):
+    for year in tqdm(years, desc='years', position=1, leave=False):
+        if var in list_pressure_vars:
+            ds = xr.open_dataset(os.path.join(root_dir, var, f'{year}.nc'))
+        else:
+            ds_2017_to_2022 = xr.open_dataset(os.path.join(root_dir, var, '2017_to_2022.nc'))
+            if year == 2017:
+                start_date = f"{year}-10-01"
+                end_date = f"{year}-12-31"
+            elif year == 2022:
+                start_date = f"{year}-01-01"
+                end_date = f"{year}-11-30"
+            else:
+                start_date = f"{year}-01-01"
+                end_date = f"{year}-12-31"
+            ds = ds_2017_to_2022.sel(time=slice(start_date, end_date))
+        
+        # chunk to smaller sizes
+        if chunk_size is not None:
+            n_chunks = len(ds.time) // chunk_size + 1
+        else:
+            n_chunks = 1
+            chunk_size = len(ds.time)
+        
+        for chunk_id in tqdm(range(n_chunks), desc='chunks', position=2, leave=False):
+            ds_small = ds.isel(time=slice(chunk_id*chunk_size, (chunk_id+1)*chunk_size))
+            if var in SINGLE_LEVEL_VARS:
+                ds_np = ds_small[NAME_TO_VAR[var]].values # N, H, W
+                normalize_mean[var].append(np.nanmean(ds_np))
+                normalize_std[var].append(np.nanstd(ds_np))
+                if var in CHEMISTRY_VARS:
+                    ds_np_log = np.log(ds_np + 1e-32)
+                    normalize_log_mean[var].append(np.nanmean(ds_np_log))
+                    normalize_log_std[var].append(np.nanstd(ds_np_log))
+            else:
+                ds_np = ds_small[NAME_TO_VAR[var]].values # N, Levels, H, W
+                try:
+                    levels_in_ds = ds.level.values
+                except:
+                    levels_in_ds = ds.isobaricInhPa.values
+                levels_in_ds = [int(l) for l in levels_in_ds]
+                assert np.sum(np.array(DEFAULT_PRESSURE_LEVELS) - levels_in_ds) == 0 # same order of pressure levels
+                for i, level in enumerate(levels_in_ds):
+                    ds_np_lev = ds_np[:, i]
+                    normalize_mean[f'{var}_{level}'].append(np.nanmean(ds_np_lev))
+                    normalize_std[f'{var}_{level}'].append(np.nanstd(ds_np_lev))
+                    if var in CHEMISTRY_VARS:
+                        ds_np_lev_log = np.log(ds_np_lev + 1e-32)
+                        normalize_log_mean[f'{var}_{level}'].append(np.nanmean(ds_np_lev_log))
+                        normalize_log_std[f'{var}_{level}'].append(np.nanstd(ds_np_lev_log))
+        
+    if var in SINGLE_LEVEL_VARS:
+        mean_over_files, std_over_files = np.array(normalize_mean[var]), np.array(normalize_std[var])
+        # var(X) = E[var(X|Y)] + var(E[X|Y])
+        variance = (std_over_files**2).mean() + (mean_over_files**2).mean() - mean_over_files.mean()**2
+        std = np.sqrt(variance)
+        # E[X] = E[E[X|Y]]
+        mean = mean_over_files.mean()
+        normalize_mean[var] = mean.reshape([1])
+        normalize_std[var] = std.reshape([1])
+        np.savez(os.path.join(save_dir, f"normalize_mean.npz"), **normalize_mean)
+        np.savez(os.path.join(save_dir, f"normalize_std.npz"), **normalize_std)
+        
+        if var in CHEMISTRY_VARS:
+            mean_log_over_files, std_log_over_files = np.array(normalize_log_mean[var]), np.array(normalize_log_std[var])
+            variance_log = (std_log_over_files**2).mean() + (mean_log_over_files**2).mean() - mean_log_over_files.mean()**2
+            std_log = np.sqrt(variance_log)
+            mean_log = mean_log_over_files.mean()
+            normalize_log_mean[var] = mean_log.reshape([1])
+            normalize_log_std[var] = std_log.reshape([1])
+            np.savez(os.path.join(save_dir, f"normalize_log_mean.npz"), **normalize_log_mean)
+            np.savez(os.path.join(save_dir, f"normalize_log_std.npz"), **normalize_log_std)
+    else:
+        for l in DEFAULT_PRESSURE_LEVELS:
+            var_lev = f'{var}_{l}'
+            mean_over_files, std_over_files = np.array(normalize_mean[var_lev]), np.array(normalize_std[var_lev])
+            # var(X) = E[var(X|Y)] + var(E[X|Y])
+            variance = (std_over_files**2).mean() + (mean_over_files**2).mean() - mean_over_files.mean()**2
+            std = np.sqrt(variance)
+            # E[X] = E[E[X|Y]]
+            mean = mean_over_files.mean()
+            normalize_mean[var_lev] = mean.reshape([1])
+            normalize_std[var_lev] = std.reshape([1])
+            
+            if var in CHEMISTRY_VARS:
+                mean_log_over_files, std_log_over_files = np.array(normalize_log_mean[var_lev]), np.array(normalize_log_std[var_lev])
+                variance_log = (std_log_over_files**2).mean() + (mean_log_over_files**2).mean() - mean_log_over_files.mean()**2
+                std_log = np.sqrt(variance_log)
+                mean_log = mean_log_over_files.mean()
+                normalize_log_mean[var_lev] = mean_log.reshape([1])
+                normalize_log_std[var_lev] = std_log.reshape([1])
+    
+        np.savez(os.path.join(save_dir, f"normalize_mean.npz"), **normalize_mean)
+        np.savez(os.path.join(save_dir, f"normalize_std.npz"), **normalize_std)
+        if var in CHEMISTRY_VARS:
+            np.savez(os.path.join(save_dir, f"normalize_log_mean.npz"), **normalize_log_mean)
+            np.savez(os.path.join(save_dir, f"normalize_log_std.npz"), **normalize_log_std)
+
+np.savez(os.path.join(save_dir, f"normalize_mean.npz"), **normalize_mean)
+np.savez(os.path.join(save_dir, f"normalize_std.npz"), **normalize_std)
+np.savez(os.path.join(save_dir, f"normalize_log_mean.npz"), **normalize_log_mean)
+np.savez(os.path.join(save_dir, f"normalize_log_std.npz"), **normalize_log_std)
---
+++ atmos_arena/data_processing/nc_to_h5df_cams.py
@@ -0,0 +1,129 @@
+import os
+import argparse
+import numpy as np
+import xarray as xr
+import h5py
+from tqdm import tqdm
+from atmos_utils.data_utils import (
+    SINGLE_LEVEL_VARS,
+    PRESSURE_LEVEL_VARS,
+    DEFAULT_PRESSURE_LEVELS,
+    NAME_TO_VAR,
+)
+
+def create_one_step_dataset(root_dir, save_dir, split, years, list_vars, chunk_size=None):
+    save_dir_split = os.path.join(save_dir, split)
+    os.makedirs(save_dir_split, exist_ok=True)
+    
+    list_single_vars = [v for v in list_vars if v in SINGLE_LEVEL_VARS]
+    list_pressure_vars = [v for v in list_vars if v in PRESSURE_LEVEL_VARS]
+    
+    for year in tqdm(years, desc='years', position=0):
+        ds_sample = xr.open_dataset(os.path.join(root_dir, list_pressure_vars[0], f'{year}.nc'))
+        if chunk_size is not None:
+            n_chunks = len(ds_sample.time) // chunk_size + 1
+        else:
+            n_chunks = 1
+            chunk_size = len(ds_sample.time)
+        
+        idx_in_year = 0
+        
+        ds_dict = {}
+        for var in list_pressure_vars:
+            ds_dict[var] = xr.open_dataset(os.path.join(root_dir, var, f'{year}.nc'))
+        for var in list_single_vars:
+            ds_2017_to_2022 = xr.open_dataset(os.path.join(root_dir, var, '2017_to_2022.nc'))
+            if year == 2017:
+                start_date = f"{year}-10-01"
+                end_date = f"{year}-12-31"
+            elif year == 2022:
+                start_date = f"{year}-01-01"
+                end_date = f"{year}-11-30"
+            else:
+                start_date = f"{year}-01-01"
+                end_date = f"{year}-12-31"
+            ds_dict[var] = ds_2017_to_2022.sel(time=slice(start_date, end_date))
+
+        for chunk_id in tqdm(range(n_chunks), desc='chunks', position=1, leave=False):
+            dict_np = {}
+            list_time_stamps = None
+            ### convert ds to numpy
+            for var in (list_single_vars + list_pressure_vars):
+                ds = ds_dict[var].isel(time=slice(chunk_id*chunk_size, (chunk_id+1)*chunk_size))
+                if list_time_stamps is None:
+                    list_time_stamps = ds.time.values
+                if var in list_single_vars:
+                    dict_np[var] = ds[NAME_TO_VAR[var]].values
+                else:
+                    try:
+                        available_levels = ds.isobaricInhPa.values
+                    except:
+                        available_levels = ds.level.values
+                    ds_np = ds[NAME_TO_VAR[var]].values
+                    for i, level in enumerate(available_levels):
+                        if level in DEFAULT_PRESSURE_LEVELS:
+                            dict_np[f'{var}_{level}'] = ds_np[:, i]
+                    
+            for i in tqdm(range(len(list_time_stamps)), desc='time stamps', position=2, leave=False):
+                data_dict = {
+                    'input': {'time': str(list_time_stamps[i])}
+                }
+                for var in dict_np.keys():
+                    data_dict['input'][var] = dict_np[var][i]
+                    
+                with h5py.File(os.path.join(save_dir_split, f'{year}_{idx_in_year:04}.h5'), 'w', libver='latest') as f:
+                    for main_key, sub_dict in data_dict.items():
+                        # Create a group for the main key (e.g., 'input' or 'output')
+                        group = f.create_group(main_key)
+                        
+                        # Now, save each array in the sub-dictionary to this group
+                        for sub_key, array in sub_dict.items():
+                            if sub_key != 'time':
+                                group.create_dataset(sub_key, data=array, compression=None, dtype=np.float32)
+                            else:
+                                group.create_dataset(sub_key, data=array, compression=None)
+                
+                idx_in_year += 1
+        
+list_vars = [
+    "2m_temperature",
+    "10m_u_component_of_wind",
+    "10m_v_component_of_wind",
+    "mean_sea_level_pressure",
+    "particulate_matter_10um",
+    "particulate_matter_1um",
+    "particulate_matter_2.5um",
+    "total_column_carbon_monoxide",
+    "total_column_nitrogen_dioxide",
+    "total_column_nitrogen_monoxide",
+    "total_column_ozone",
+    "total_column_sulphur_dioxide",
+    
+    # "carbon_monoxide",
+    # "geopotential",
+    # "nitrogen_dioxide",
+    # "nitrogen_monoxide",
+    "ozone",
+    "specific_humidity",
+    "sulphur_dioxide",
+    # "temperature",
+    "u_component_of_wind",
+    "v_component_of_wind",
+]
+
+parser = argparse.ArgumentParser()
+    
+parser.add_argument("--start", type=int, required=True)
+parser.add_argument("--end", type=int, required=True)
+parser.add_argument("--split", type=str, required=True)
+
+args = parser.parse_args()
+        
+create_one_step_dataset(
+    root_dir='/eagle/MDClimSim/tungnd/data/cams_1.40625',
+    save_dir='/eagle/MDClimSim/tungnd/data/cams_1.40625_one_step_h5df',
+    split=args.split,
+    years=list(range(args.start, args.end)),
+    list_vars=list_vars,
+    chunk_size=100
+)
